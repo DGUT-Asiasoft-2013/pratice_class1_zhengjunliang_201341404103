e13b2d832607afdcbd9f12db8a7ffefa06b31b0e
Administrator <Administrator@7A304-0030>
2016-12-14 16:29:48 +0800

    添加对文章的评价功能

--- src/com/example/helloworld/fragments/CommentListFragment.java
+++ src/com/example/helloworld/fragments/CommentListFragment.java
@@ -30,7 +30,7 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
-public class CommentListFragment extends Fragment {
+public class CommentListFragment extends Fragment {  
 
 	View view;
 	ListView listView;
